3ddadb64bc9cbfa2a7dff5a9d7eb84ec655cb7f3
karrar mohammed <karrar.m.habeeb@gmail.com>
2026-08-11 01:23:56 +0300

    fix jackson dependency and setup database environment variables

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("tools.jackson.module:jackson-module-kotlin")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     runtimeOnly("org.postgresql:postgresql")
     
     testImplementation("org.springframework.boot:spring-boot-starter-test")
